b9f6f53ce3011934fa04088e673880445f27b930
NatalkaPron <natalka-pronina@mail.ru>
2023-03-06 17:00:20 +0300

    Convert Indentation to Spaces

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -4,38 +4,38 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("message", 
-					help="what will the cow say", 
-					nargs='?', 
-					default="", 
-					type=str)
+                    help="what will the cow say", 
+                    nargs='?', 
+                    default="", 
+                    type=str)
 
 parser.add_argument("-e", "--eye_string", 
-					help="select the appearance of the cow's eyes", 
-					default="oo", 
-					type=str)
+                    help="select the appearance of the cow's eyes", 
+                    default="oo", 
+                    type=str)
 
 parser.add_argument("-f", "--cowfile", 
-					help="""contains '/' => path; else => search in the COWPATH""", 
-					default="default", 
-					type=str)
+                    help="""contains '/' => path; else => search in the COWPATH""", 
+                    default="default", 
+                    type=str)
 
 parser.add_argument("-l", "--list", 
-					help="list of all cowfiles", 
-					action="store_true")
+                    help="list of all cowfiles", 
+                    action="store_true")
 
 parser.add_argument("-n", "--wrap_text", 
-					help="wrap_text", 
-					action='store_false')
+                    help="wrap_text", 
+                    action='store_false')
 
 parser.add_argument("-T", "--tongue_string", 
-					help="select the appearance of the cow's tongue", 
-					default="  ", 
-					type=str)
+                    help="select the appearance of the cow's tongue", 
+                    default="  ", 
+                    type=str)
 
 parser.add_argument("-W", "--column", 
-					help="eyes", 
-					default=40, 
-					type=int)
+                    help="eyes", 
+                    default=40, 
+                    type=int)
 
 parser.add_argument("-b", "--borg", 	help="borg", 		action="store_true")
 parser.add_argument("-d", "--dead", 	help="dead", 		action="store_true")
@@ -53,33 +53,33 @@ args.eye_string = args.eye_string[:2]
 args.tongue_string = args.tongue_string[:2]
 
 if "/" in args.cowfile or args.cowfile[-4:] == ".cow":
-	args.cow = "default"
-	with open(args.cowfile) as file:
-		args.cowfile = read_dot_cow(file)
+    args.cow = "default"
+    with open(args.cowfile) as file:
+        args.cowfile = read_dot_cow(file)
 else:
-	args.cow = args.cowfile
-	args.cowfile = None
-	
+    args.cow = args.cowfile
+    args.cowfile = None
+    
 
 d = {	"b": args.borg, 
-		"d": args.dead, 
-		"g": args.greedy, 
-		"p": args.paranoia, 
-		"s": args.stoned,
-		"t": args.tired,
-		"w": args.wakeup,
-		"y": args.youthful}
+        "d": args.dead, 
+        "g": args.greedy, 
+        "p": args.paranoia, 
+        "s": args.stoned,
+        "t": args.tired,
+        "w": args.wakeup,
+        "y": args.youthful}
 
 preset = sorted(d.items(), key=lambda x: (x[1], x[0]))
 preset = preset[-1]
 if preset[1]:
-	preset = preset[0]
+    preset = preset[0]
 else:
-	preset = None
+    preset = None
 
 if args.list:
-	print(list_cows())
+    print(list_cows())
 else:
-	print(cowsay(	args.message, cow=args.cow, 
-					preset=preset, eyes=args.eye_string, tongue=args.tongue_string, 
-					width=args.column, wrap_text=True, cowfile=args.cowfile))
+    print(cowsay(	args.message, cow=args.cow, 
+                    preset=preset, eyes=args.eye_string, tongue=args.tongue_string, 
+                    width=args.column, wrap_text=True, cowfile=args.cowfile))
